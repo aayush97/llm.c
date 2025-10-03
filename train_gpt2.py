640b0ac8a85d5d404282f68a9554f7ec307813c5
aayush97 <agautam1997@gmail.com>
2025-10-03 15:26:35 -0500

    Replace layernorm with rmsnorm

--- train_gpt2.py
+++ train_gpt2.py
@@ -109,9 +109,9 @@ class Block(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.ln_1 = nn.RMSNorm(config.n_embd, eps=1e-6)
         self.attn = CausalSelfAttention(config)
-        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.ln_2 = nn.RMSNorm(config.n_embd, eps=1e-6)
         self.mlp = MLP(config)
 
     def forward(self, x):
